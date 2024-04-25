fe5c92e88c265a6f4f6c41d1047407d1c3cc177f
Fernando Raphael Keller <fkeller_3@hotmail.com>
2024-04-25 16:03:27 -0300

    Validado assessor na listagem do cliente caso o usuário logado não seja um Admin

--- Xp.FinancialPortfolioManager.Application/Common/Interfaces/IAdvisorsRepository.cs
+++ Xp.FinancialPortfolioManager.Application/Common/Interfaces/IAdvisorsRepository.cs
@@ -6,6 +6,7 @@ namespace Xp.FinancialPortfolioManager.Application.Common.Interfaces
     {
         Task AddAdvisorAsync(Advisor advisor);
         Task<Advisor?> GetByIdAsync(Guid advisorId);
+        Task<Advisor?> GetByUserIdAsync(Guid userId);
         Task<List<Advisor>> ListAdvisors();
         Task UpdateAsync(Advisor advisor);
     }
