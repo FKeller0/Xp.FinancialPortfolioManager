43b97e4f39a7a0de516ee01a2abdf8f59fdd0f1d
Fernando Raphael Keller <fkeller_3@hotmail.com>
2024-04-25 15:49:26 -0300

    Migrations

---
+++ Xp.FinancialPortfolioManager.Infrastructure/Migrations/20240425174620_Initial-Create.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Xp.FinancialPortfolioManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Advisors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Advisors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    AdvisorId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    ClientId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    AdminId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    AdvisorId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Balance = table.Column<double>(type: "REAL", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clients_Advisors_AdvisorId",
+                        column: x => x.AdvisorId,
+                        principalTable: "Advisors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Admins",
+                column: "Id",
+                value: new Guid("2150e333-8fdc-42a3-9474-1a3956d46de8"));
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "AdminId", "AdvisorId", "ClientId", "Email", "FirstName", "LastName", "PasswordHash" },
+                values: new object[] { new Guid("1b41fae8-e02c-448b-944d-6169ebc26ddf"), new Guid("2150e333-8fdc-42a3-9474-1a3956d46de8"), null, null, "financialportfolio@xpfinancialportfolio.com", "Sys", "Admin", "$2a$11$fm.DCQn2.Cq7PlPfhwWDuO7pgEjGqs0pCf1P5Q24VejUqc.XxahLm" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_AdvisorId",
+                table: "Clients",
+                column: "AdvisorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Advisors");
+        }
+    }
+}
